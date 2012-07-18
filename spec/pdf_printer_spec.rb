c9bc6dfcf17fda215db33d140a6c5389a5181017
Pablo Vicente <vicapa99@gmail.com>
2012-07-18 23:40:23 +0100

    [Refactor] move pdf to a instace variable and initialize it in each clause

--- spec/pdf_printer_spec.rb
+++ spec/pdf_printer_spec.rb
@@ -3,14 +3,17 @@ require './pdf_printer'
 require 'prawn/measurement_extensions'
 
 describe 'pdf printer' do
+
+  before(:each) do
+    @pdf = PdfPrinter.new
+  end
+
   context 'default values' do
     it 'has 450 mm width' do
-      pdf = PdfPrinter.new
-      pdf.bounds.width.should == 450.mm
+      @pdf.bounds.width.should == 450.mm
     end
     it 'has 320 mm height' do
-      pdf = PdfPrinter.new
-      pdf.bounds.height.should == 320.mm
+      @pdf.bounds.height.should == 320.mm
     end
   end
 end
